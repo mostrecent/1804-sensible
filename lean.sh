a6b37c5565e2f51d7d495176dac69865d1657bb6
mostrecent <46225210+mostrecent@users.noreply.github.com>
2019-04-16 10:51:40 +0200

    add docker

--- lean.sh
+++ lean.sh
@@ -47,3 +47,25 @@ wS 'ln -s $HOME/.config/tmux/.tmux.conf $HOME/.tmux.conf'
 #
 # Set root and user password with passwd and passwd USER as root
 
+
+# Docker install as root
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io
+
+usermod -aG docker $USER
+
